d9850521d1870ce0e5fa5ac531fea5f2495cb6a2
shivamgupta001 <shivamcs3080@gmail.com>
2017-09-27 00:26:50 +0530

    added ffmpeg

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const electron = require('electron');
-const { app, BrowserWindow } = electron;
+const ffmpeg = require('fluent-ffmpeg');
+const { app, BrowserWindow, ipcMain } = electron;
 
 app.on('ready', () => {
 	console.log("Electron application is running");
 	const mainWindow = new BrowserWindow({});
 	mainWindow.loadURL(`file://${__dirname}/index.html`);
+	
+});
+
+ipcMain.on('video:submit', (event , path) => {
+	ffmpeg.ffprobe(path , (err, metadata) => {
+		console.log(metadata);
+	})
 });
